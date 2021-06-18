4f11b0053694d3dba5e97adb91effc825d2f117d
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-06-18 15:46:23 -0500

    Update variable.py

---
+++ fuzzy-expert/variable.py
@@ -0,0 +1,16 @@
+class FuzzyVariable:
+    """Creates a fuzzy variable.
+
+    Args:
+        name (string): variable name.
+        universe (list, numpy.array): list of points defining the universe of the variable.
+        sets (dict): dictionary where keys are the name of the sets, and the values correspond to the membership for each point of the universe.
+
+    Returns:
+        A fuzzy variable.
+
+    """
+
+    def __init__(self, universe, terms):
+        self.universe = universe
+        self.terms = terms
