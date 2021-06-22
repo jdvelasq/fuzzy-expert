6d8725092d759e9a402cdcfb3f9fd7f73f01f494
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-06-22 15:54:35 -0500

    Feat: rule print

--- fuzzy_expert/rule.py
+++ fuzzy_expert/rule.py
@@ -5,50 +5,63 @@ Zadeh-Mamdani Rules
 """
 
 
-# class FuzzyRule:
-#     """Creates a Zadeh-Mamdani fuzzy rule.
+class FuzzyRule:
+    """Creates a Zadeh-Mamdani fuzzy rule.
 
-#     Args:
-#         antecedents (list of tuples): Fuzzy variables in the rule antecedent.
-#         consequent (tuple): Fuzzy variable in the consequence.
-#         is_and (bool): When True, membership values are combined using the specified AND operator; when False, the OR operator is used.
+    Args:
+        antecedents (list of tuples): Fuzzy variables in the rule antecedent.
+        consequent (tuple): Fuzzy variable in the consequence.
+        is_and (bool): When True, membership values are combined using the specified AND operator; when False, the OR operator is used.
 
-#     """
+    """
 
-#     def __init__(
-#         self,
-#         premises,
-#         consequence,
-#         cf=1.0,
-#         threshold_cf=0,
-#     ):
-#         self.premises = premises
-#         self.consequence = consequence
-#         self.rule_cf = cf
-#         self.threshold_cf = threshold_cf
+    def __init__(
+        self,
+        premises,
+        consequences,
+        cf=1.0,
+        threshold_cf=0,
+    ):
+        self.premises = premises
+        self.consequences = consequences
+        self.rule_cf = cf
+        self.threshold_cf = threshold_cf
 
-#     def __repr__(self):
+    def __repr__(self):
 
-#         text = "IF  "
-#         space = " " * 4
+        text = "IF  "
+        space = " " * 4
 
-#         for i_premise, premise in enumerate(self.premises):
+        #
+        # Premises
+        #
+        for i_premise, premise in enumerate(self.premises):
 
-#             if i_premise == 0:
-#                 text += premise[0].name + " IS"
-#                 for t in premise[1:]:
-#                     text += " " + t
-#                 text += "\n"
-#             else:
-#                 text += space + premise[0] + " " + premise[1].name + " IS"
-#                 for t in premise[2:]:
-#                     text += " " + t
-#                 text += "\n"
+            if i_premise == 0:
+                text += premise[0] + " IS"
+                for t in premise[1:]:
+                    text += " " + t
+                text += "\n"
+            else:
+                text += space + premise[0] + " " + premise[1] + " IS"
+                for t in premise[2:]:
+                    text += " " + t
+                text += "\n"
 
-#         text += "THEN\n"
-#         text += space + self.consequence[0].name + " IS"
-#         for t in self.consequence[1:]:
-#             text += " " + t + "\n"
-#         text += "CF = {:.2f}\n".format(self.rule_cf)
-#         text += "Threshold-CF = {:.2f}\n".format(self.rule_cf)
-#         return text
+        text += "THEN\n"
+
+        #
+        # Consequences
+        #
+        for consequence in self.consequences:
+            text += space + consequence[0] + " IS"
+            for t in consequence[1:]:
+                text += " " + t
+            text += "\n"
+        #
+        # Certainty factors
+        #
+        text += "CF = {:.2f}\n".format(self.rule_cf)
+        text += "Threshold-CF = {:.2f}\n".format(self.rule_cf)
+
+        return text
---
+++ tests/test_rule.py
@@ -0,0 +1,29 @@
+"""Tests for fuzzy rules"""
+
+
+from fuzzy_expert.rule import FuzzyRule
+
+
+def test_repr():
+    """
+    Test function printing.
+
+    """
+
+    rule = FuzzyRule(
+        premises=[("score", "HIGH"), ("AND", "ratio", "very", "LOW")],
+        consequences=[("decision1", "BAD"), ("decision2", "very", "GOOD")],
+    )
+
+    text = rule.__repr__()
+    assert (
+        text
+        == """IF  score IS HIGH
+    AND ratio IS very LOW
+THEN
+    decision1 IS BAD
+    decision2 IS very GOOD
+CF = 1.00
+Threshold-CF = 1.00
+"""
+    )
