54adf4dbb3e58e2bb2c0346642b6d2359699df89
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2021-06-24 17:21:31 +0000

    DOC: membership documentation

--- fuzzy_expert/mf.py
+++ fuzzy_expert/mf.py
@@ -2,7 +2,7 @@
 Membership Functions
 ==============================================================================
 
-Functions to compute fuzzy membership values for numpy.arrays.
+Functions in this module returns a standard membership function specificaion as a list of points (x_i, u_i).
 
 """
 from __future__ import annotations
@@ -14,11 +14,31 @@ import numpy as np
 
 
 class MembershipFunction:
+    """Membership function constructor.
+
+    :param n_points: Number base point for building the approximations.
+
+    >>> from fuzzy_expert.mf import MembershipFunction
+    >>> mf = MembershipFunction(n_points=3)
+    >>> mf(('gaussmf', 5, 1))
+    [(2, 0), (3.0, 0.1353352832366127), (3.8, 0.48675225595997157), (4.6, 0.9231163463866356), (5.0, 1.0), (5.4, 0.9231163463866356), (6.2, 0.48675225595997157), (7.0, 0.1353352832366127), (8, 0)]
+
+
+    """
+
     def __init__(self, n_points: int = 9):
         self.n_points: int = n_points
 
     def __call__(self, mfspec: tuple):
+        """Generates a list of poinnts representing the membership function.
 
+
+        >>> from fuzzy_expert.mf import MembershipFunction
+        >>> mf = MembershipFunction(n_points=3)
+        >>> mf(('gaussmf', 5, 1))
+        [(2, 0), (3.0, 0.1353352832366127), (3.8, 0.48675225595997157), (4.6, 0.9231163463866356), (5.0, 1.0), (5.4, 0.9231163463866356), (6.2, 0.48675225595997157), (7.0, 0.1353352832366127), (8, 0)]
+
+        """
         fn, *params = mfspec
 
         fn = {
@@ -35,14 +55,25 @@ class MembershipFunction:
         return fn(*params)
 
     def gaussmf(self, center: float, sigma: float) -> List[Tuple[float, float]]:
-        """
-        Gaussian membership function.
+        """Gaussian membership function.
+
+        :param center: Defines the center of the membership function.
+        :param sigma: Defines the width of the membership function, where a larger value creates a wider membership function.
+
+        >>> from fuzzy_expert.mf import MembershipFunction
+        >>> mf = MembershipFunction(n_points=3)
+        >>> mf.gaussmf(center=5, sigma=1)
+        [(2, 0), (3.0, 0.1353352832366127), (3.8, 0.48675225595997157), (4.6, 0.9231163463866356), (5.0, 1.0), (5.4, 0.9231163463866356), (6.2, 0.48675225595997157), (7.0, 0.1353352832366127), (8, 0)]
+
         """
         xp: np.ndarray = np.linspace(
             start=center - 2 * sigma,
             stop=center + 2 * sigma,
             num=2 * self.n_points,
         )
+        xp = np.append(xp, center)
+        xp = np.unique(xp)
+        xp.sort()
         fp: np.ndarray = np.exp(-((xp - center) ** 2) / (2 * sigma))
         return (
             [(center - 3 * sigma, 0)]
@@ -51,10 +82,21 @@ class MembershipFunction:
         )
 
     def gbellmf(
-        self, center: float, width: float, shape: float
+        self,
+        center: float,
+        width: float,
+        shape: float,
     ) -> List[Tuple[float, float]]:
-        """
-        Generalized bell-shaped membership function.
+        """Generalized bell-shaped membership function.
+
+        :param center: Defines the center of the membership function.
+        :param width: Defines the width of the membership function, where a larger value creates a wider membership function.
+        :param shape: Defines the shape of the curve on either side of the central plateau, where a larger value creates a more steep transition.
+
+        >>> from fuzzy_expert.mf import MembershipFunction
+        >>> mf = MembershipFunction(n_points=3)
+        >>> mf.gbellmf(center=5, width=1, shape=0.5)
+        [(-1, 0), (0.0, 0.16666666666666666), (1.0, 0.2), (2.0, 0.25), (3.0, 0.3333333333333333), (3.8, 0.45454545454545453), (4.0, 0.5), (4.6, 0.7142857142857141), (5.0, 1.0), (5.4, 0.7142857142857141), (6.0, 0.5), (6.2, 0.45454545454545453), (7.0, 0.3333333333333333), (8.0, 0.25), (9.0, 0.2), (10.0, 0.16666666666666666), (11, 0)]
 
         """
         xp: np.ndarray = np.linspace(
@@ -78,12 +120,21 @@ class MembershipFunction:
         right_peak: float,
         right_feet: float,
     ) -> List[Tuple[float, float]]:
-        """
-        Pi-shaped membership function.
+        """Pi-shaped membership function.
+
+        :param left_feet: Defines the left feet of the membership function.
+        :param left_peak: Defines the left peak of the membership function.
+        :param right_peak: Defines the right peak of the membership function.
+        :param right_feet: Defines the right feet of the membership function.
+
+        >>> from fuzzy_expert.mf import MembershipFunction
+        >>> mf = MembershipFunction(n_points=4)
+        >>> mf.pimf(left_feet=1, left_peak=2, right_peak=3, right_feet=4)
+        [(1.0, 0.0), (1.3333333333333333, 0.22222222222222213), (1.6666666666666665, 0.7777777777777776), (2.0, 1.0), (3.0, 1.0), (3.3333333333333335, 0.7777777777777776), (3.6666666666666665, 0.22222222222222243), (4.0, 0.0)]
 
         """
         return self.smf(foot=left_feet, shoulder=left_peak) + self.zmf(
-            a=right_peak, b=right_feet
+            shoulder=right_peak, feet=right_feet
         )
 
     def sigmf(
@@ -91,23 +142,44 @@ class MembershipFunction:
         center: float,
         width: float,
     ) -> List[Tuple[float, float]]:
-        """
-        Sigmoidal membership function.
+        """Sigmoidal membership function.
+
+        :param center: Defines the center of the membership function.
+        :param width: Defines the width of the membership function.
+
+        >>> from fuzzy_expert.mf import MembershipFunction
+        >>> mf = MembershipFunction(n_points=3)
+        >>> mf.sigmf(center=5, width=1)
+        [(-1, 0), (0.0, 0.0066928509242848554), (2.0, 0.04742587317756678), (4.0, 0.2689414213699951), (5.0, 0.5), (6.0, 0.7310585786300049), (8.0, 0.9525741268224334), (10.0, 0.9933071490757153), (11, 1)]
 
         """
         xp: np.ndarray = np.linspace(
             start=center - 5 * width, stop=center + 5 * width, num=2 * self.n_points
         )
+        xp: np.ndarray = np.append(xp, center)
+        xp = np.unique(xp)
+        xp.sort()
         fp: np.ndarray = 1 / (1 + np.exp(-np.abs(width) * (xp - center)))
-        return [(x, f) for x, f in zip(xp, fp)]
+        return (
+            [(center - 6 * width, 0)]
+            + [(x, f) for x, f in zip(xp, fp)]
+            + [(center + 6 * width, 1)]
+        )
 
     def smf(
         self,
         foot: float,
         shoulder: float,
     ) -> List[Tuple[float, float]]:
-        """
-        S-shaped membership function.
+        """S-shaped membership function.
+
+        :param foot: Defines the foot of the membership function.
+        :param shoulder: Defines the shoulder of the membership function.
+
+        >>> from fuzzy_expert.mf import MembershipFunction
+        >>> mf = MembershipFunction(n_points=4)
+        >>> mf.smf(foot=1, shoulder=2)
+        [(1.0, 0.0), (1.3333333333333333, 0.22222222222222213), (1.6666666666666665, 0.7777777777777776), (2.0, 1.0)]
 
         """
         xp: np.ndarray = np.linspace(start=foot, stop=shoulder, num=self.n_points)
@@ -133,8 +205,18 @@ class MembershipFunction:
         right_peak: float,
         right_feet: float,
     ) -> List[Tuple[float, float]]:
-        """
-        Trapezoidal membership function.
+        """Trapezoidal membership function.
+
+        :param left_feet: Defines the left feet of the membership function.
+        :param left_peak: Defines the left peak of the membership function.
+        :param right_peak: Defines the right peak of the membership function.
+        :param right_feet: Defines the right feet of the membership function.
+
+        >>> from fuzzy_expert.mf import MembershipFunction
+        >>> mf = MembershipFunction(n_points=4)
+        >>> mf.trapmf(left_feet=1, left_peak=2, right_peak=3, right_feet=4)
+        [(1.0, 0.0), (2.0, 1.0), (3.0, 1.0), (4.0, 0.0)]
+
 
         """
         left_feet: np.ndarray = np.where(
@@ -169,8 +251,16 @@ class MembershipFunction:
         peak: float,
         right_feet: float,
     ) -> List[Tuple[float, float]]:
-        """
-        Triangular membership function.
+        """Triangular membership function.
+
+        :param left_feet: Defines the left feet of the membership function.
+        :param peak: Defines the peak of the membership function.
+        :param right_feet: Defines the right feet of the membership function.
+
+        >>> from fuzzy_expert.mf import MembershipFunction
+        >>> mf = MembershipFunction(n_points=4)
+        >>> mf.trimf(left_feet=1, peak=2, right_feet=4)
+        [(1.0, 0.0), (2.0, 1.0), (4.0, 0.0)]
 
         """
         left_feet: np.ndarray = np.where(left_feet == peak, left_feet - 1e-4, left_feet)
@@ -191,21 +281,28 @@ class MembershipFunction:
 
     def zmf(
         self,
-        a: float,
-        b: float,
+        shoulder: float,
+        feet: float,
     ) -> List[Tuple[float, float]]:
-        """
-        Z-shaped membership function.
+        """Z-shaped membership function.
+
+        :param shoulder: Defines the shoulder of the membership function.
+        :param feet: Defines the feet of the membership function.
+
+        >>> from fuzzy_expert.mf import MembershipFunction
+        >>> mf = MembershipFunction(n_points=4)
+        >>> mf.zmf(shoulder=1, feet=2)
+        [(1.0, 1.0), (1.3333333333333333, 0.7777777777777779), (1.6666666666666665, 0.22222222222222243), (2.0, 0.0)]
 
         """
-        xp: np.ndarray = np.linspace(start=a, stop=b, num=self.n_points)
+        xp: np.ndarray = np.linspace(start=shoulder, stop=feet, num=self.n_points)
         fp: np.ndarray = np.where(
-            xp <= a,
+            xp <= shoulder,
             1,
             np.where(
-                xp <= (a + b) / 2,
-                1 - 2 * ((xp - a) / (b - a)) ** 2,
-                np.where(xp <= b, 2 * ((xp - b) / (b - a)) ** 2, 0),
+                xp <= (shoulder + feet) / 2,
+                1 - 2 * ((xp - shoulder) / (feet - shoulder)) ** 2,
+                np.where(xp <= feet, 2 * ((xp - feet) / (feet - shoulder)) ** 2, 0),
             ),
         )
         return [(x, f) for x, f in zip(xp, fp)]
